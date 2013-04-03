ca39757e81645150be5f55526c4dd40817f63249
toaster33 <fwhorton3@yahoo.com>
2013-04-02 22:48:25 -0400

    Add logback to project.  Add logging to HomeController.

--- SampleApplication/src/main/java/com/sample/mvc/HomeController.java
+++ SampleApplication/src/main/java/com/sample/mvc/HomeController.java
@@ -1,6 +1,8 @@
 package com.sample.mvc;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +13,22 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class HomeController {
-	
-	@RequestMapping(value = "/compare")
-	public String compare(@RequestParam("input1") String first, @RequestParam("input2") String second, Model model) {
-		int result = StringUtils.length(first) - StringUtils.length(second);
-        String inEnglish = (result < 0) ? "less than" : (result > 0 ? "greater than" : "equal to");
-        String output = "According to our Comparator, '" + first + "' is " + inEnglish + "'" + second+ "'";
 
-        model.addAttribute("output", output);
-        return "compareResult";
+	static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/compare")
+	public String compare(@RequestParam(value = "input1", required = true) String first,
+			@RequestParam(value ="input2", required = true) String second, Model model) {
+		LOG.debug("compare.html, parameters: {}, {}", first, second);
+
+		int result = StringUtils.length(first) - StringUtils.length(second);
+		String inEnglish = (result < 0) ? "less than"
+				: (result > 0 ? "greater than" : "equal to");
+		String output = "According to our Comparator, '" + first + "' is "
+				+ inEnglish + "'" + second + "'";
+
+		model.addAttribute("output", output);
+		return "compareResult";
 	}
 
 }
